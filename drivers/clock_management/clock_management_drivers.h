c5c838ffd56b701348c90af76014baeb144f82df
Mathieu Choplain <mathieu.choplain@st.com>
2025-05-23 13:03:02 +0200

    Add clock gate
    Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>

--- drivers/clock_management/clock_management_drivers.h
+++ drivers/clock_management/clock_management_drivers.h
@@ -22,6 +22,9 @@ extern "C" {
 #define Z_CLOCK_MANAGEMENT_CLOCK_SOURCE_DATA_GET(node_id, prop, idx)      \
 	DT_PHA_BY_IDX(node_id, prop, idx, gate)
 
+#define Z_CLOCK_MANAGEMENT_CLOCK_GATE_DATA_DEFINE(node_id, prop, idx)
+#define Z_CLOCK_MANAGEMENT_CLOCK_GATE_DATA_GET(node_id, prop, idx)		\
+	DT_PHA_BY_IDX(node_id, prop, idx, gate_enable)
 
 /* pow2-prescaler: data is value to program in register field (= LOG2(prescaler)) */
 #define Z_CLOCK_MANAGEMENT_POW2_PRESCALER_DATA_DEFINE(node_id, prop, idx)	\
