f08d698bfce6c154034e901232bf81ff9dd9a60f
Mathieu Choplain <mathieu.choplain@st.com>
2025-05-23 13:40:57 +0200

    drivers: clock_management: stm32: add infrastructure
    Add infrastructure to integrate STM32-specific clock management drivers
    into the subsystem.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>

---
+++ drivers/clock_management/stm32/clock_management_stm32.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_STM32_CLOCK_MANAGEMENT_STM32_H_
+#define ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_STM32_CLOCK_MANAGEMENT_STM32_H_
+
+#include <zephyr/devicetree.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @cond INTERNAL_HIDDEN */
+
+/** @endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_STM32_CLOCK_MANAGEMENT_STM32_H_ */
