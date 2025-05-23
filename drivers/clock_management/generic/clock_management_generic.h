affd99c4523c6c913c078535fbeef6da3535b826
Mathieu Choplain <mathieu.choplain@st.com>
2025-05-23 11:50:42 +0200

    drivers: clock_management: add infrastructure for generic clock drivers
    Create basic skeleton that allows integration of clock management drivers
    for "generic" elements. This can be real hardware drivers, for elements
    that are common across many SoC vendors and share the same MMIO interface,
    or drivers that merely represent hardware and need not interact with any
    MMIO interface.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>

---
+++ drivers/clock_management/generic/clock_management_generic.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_GENERIC_H_
+#define ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_GENERIC_H_
+
+#include <zephyr/devicetree.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @cond INTERNAL_HIDDEN */
+
+/* Macro definitions for generic clock drivers */
+
+/** @endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_DRIVERS_CLOCK_MANAGEMENT_GENERIC_H_ */
